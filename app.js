e4f967e28256547185f5b3c8abbaef60141e997d
MasReed <bluekitdrummer@gmail.com>
2021-03-03 10:44:18 -0600

    Store email and pw only in database

--- app.js
+++ app.js
@@ -15,6 +15,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
+// MongoDB Connection & Schemas ////////////////////////////////////////////////
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = {
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}
+
+const User = new mongoose.model('User', userSchema);
+
+
+// Express Page Routing ////////////////////////////////////////////////////////
+
 // Home route
 app.get('/', function(req, res){
     res.render('home.ejs');
@@ -22,18 +44,53 @@ app.get('/', function(req, res){
 
 
 // Login route
-app.get('/login', function(req, res){
-    res.render('login.ejs');
-});
+app.route('/login')
+
+    .get(function(req, res){
+        res.render('login.ejs');
+    })
+
+    .post(function(req, res){
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username}, function(err, foundUser){
+            if (err) {
+                console.log(err);
+            } else if (foundUser) {
+                if (foundUser.password === password){
+                    res.render('secrets.ejs');
+                }
+            }
+        });
+    });
 
 
 // Register route
-app.get('/register', function(req, res){
-    res.render('register.ejs');
-});
+app.route('/register')
+
+    .get(function(req, res){
+        res.render('register.ejs');
+    })
+
+    .post(function(req, res){
+
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets.ejs');
+            }
+        });
+    });
 
 
-// Secrets route
+// TEMP Secrets route
 app.get('/secrets', function(req, res){
     res.render('secrets.ejs');
 });
